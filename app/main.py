fbdbc1ed049ac194974519162ca987e85036f8c1
X00201548 Matteo Coletti <x00201548@TA.IT-TALLAGHT.IE>
2024-10-08 13:43:12 +0100

    lab week 4

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI, APIRouter
 from fastapi.staticfiles import StaticFiles
 import httpx
 from contextlib import asynccontextmanager
-
+from app.routes.todo_routes import router as todo_router
 from app.routes.home_routes import router as home_router
 
 main_router = APIRouter()
-
+main_router.include_router(todo_router, prefix="/todo", tags=["todo"])
 main_router.include_router(home_router)
 
 
@@ -30,3 +30,7 @@ app.mount(
     StaticFiles(directory="app/static"),
     name="static",
 )
+
+# include routes in app
+app.include_router(main_router)
+
